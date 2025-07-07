432f374198a3cf83b8829dde8c9ada99ce14c2f8
yDenBarbosa <denis9195@hotmail.com>
2025-07-07 16:03:26 -0300

    feat: more functions

--- aula_07-07/function.js
+++ aula_07-07/function.js
@@ -30,8 +30,8 @@ fazerSuco("Morango")
 
 let n1 = 25
 let n2 = 19
-let result = n1 + n2
-console.log(result)
+let result1 = n1 + n2
+console.log(result1)
 
 let n3 = 10
 let n4 = 51
@@ -47,3 +47,40 @@ function somar(p1, p2){
     console.log(`Resultado da soma: ${p1 + p2}`)
 }
 somar(29, 5)
+
+
+// console.log("Começando")
+
+// setTimeout(function(){
+//     console.log("Terminando")
+// }, 2000)
+
+
+
+function dobro(numero){
+    return numero * 2
+}
+
+let result = dobro(2)
+console.log(result)
+
+function quadrado(numero){
+    return numero * numero
+}
+console.log(quadrado(4))
+
+
+//------------------------ Arrow Functions -----------------------------//
+
+// const dobrar = (numero) =>{
+//     console.log(numero * 2)
+// }
+
+// dobrar(10)
+
+const dobrar = (n) => n * 2
+let resultado1 = dobrar(6)
+console.log(resultado1)
+
+const texto = (tx) => console.log(`Saia ${tx}`)
+texto("Primata")
