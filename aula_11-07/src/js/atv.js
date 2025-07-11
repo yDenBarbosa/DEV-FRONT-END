107cd00f8c23cdfdf078947cd3b8ded306c185cb
unset <unset>
2025-07-11 14:19:55 -0300

    feat:atv

---
+++ aula_11-07/src/js/atv.js
@@ -0,0 +1,7 @@
+let mensagem = document.getElementById("mensagem")
+
+// mensagem.innerText = "Esse texto mundou de visual!"
+mensagem.style.color = "red"
+mensagem.style.fontSize = "86px"
+mensagem.style.backgroundColor = "yellow"
+mensagem.style.border = "2px dashed gray"
